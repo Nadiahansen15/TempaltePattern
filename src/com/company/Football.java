1619f3ea5705a977de06770b9daa0061228144c4
Nadia Hansen <nadiahansen.kea@gmail.com>
2020-06-07 11:26:05 +0200

    Initial commit

---
+++ src/com/company/Football.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Football extends Game {
+
+    @Override //lige meget hvad metoden kunne før, skal den her override
+    void endPlay() {
+        System.out.println("Football Game Finished!");
+    }
+
+    @Override
+    void initialize() {
+        System.out.println("Football Game Initialized! Start playing.");
+    }
+
+    @Override
+    void startPlay() {
+        System.out.println("Football Game Started. Enjoy the game!");
+    }
+}
---
+++ src/com/company/Game.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public abstract class Game {
+
+    // The body is provided by the subclass (inherited from).
+    abstract void initialize();
+    abstract void startPlay();
+    abstract void endPlay();
+
+    //template method
+    public final void play(){
+
+        //initialize the game
+        initialize();
+
+        //start game
+        startPlay();
+
+        //end game
+        endPlay();
+    }
+}
